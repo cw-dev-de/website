f5a340945db4293227fa27fe4e9a9ce19089dd3e
ChristianWegner <christian.wegner14w@gmail.com>
2026-09-11 19:20:39 +0200

    new

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Hi, ich bin Christian</h1>
+      <p>Azubi im 3. Lehrjahr & Anwendungsentwickler</p>
+    </section>
+  );
+}
+export default Hero;
